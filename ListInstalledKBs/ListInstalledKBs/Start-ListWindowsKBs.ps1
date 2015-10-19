830f237ad8f54369130b1e185c3b7f1348cbc774
sebmatthews <seb@sebmatthews.net>
2015-10-19 19:58:04 +0100

    master

---
+++ ListInstalledKBs/ListInstalledKBs/Start-ListWindowsKBs.ps1
@@ -0,0 +1,17 @@
+﻿Function Start-ListInstalledKBs {
+	$updateobject = New-Object -comobject “Microsoft.Update.Searcher”
+	$allupdates = $updateobject.QueryHistory(0,$updateobject.GetTotalHistoryCount())
+	$KBoutput = @()
+	$Regexpattern = “KB\d*”
+	Foreach ($update in $allupdates) {
+	    $KBname = $update.Title
+	    $KBid = $KBname | Select-String -Pattern $Regexpattern
+		$KBoutput += New-Object PSObject -Property @{
+		   	Title = $KBname
+			KB = $KBid.Matches.Value
+		}
+	}
+	($KBoutput | Sort-Object KB) | Export-Csv $ENV:USERPROFILE"\desktop\installedKBs.csv" -NoTypeInformation
+}
+
+Start-ListInstalledKBs
